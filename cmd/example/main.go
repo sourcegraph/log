a27ef1bbe2629de109b7e90fcc4bf6c6a32395fb
Robert Lin <robert@bobheadxi.dev>
2022-12-05 12:12:52 -0800

    add example program

---
+++ cmd/example/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"os"
+	"time"
+
+	"github.com/sourcegraph/log"
+)
+
+func main() {
+	liblog := log.Init(log.Resource{
+		Name: "logexample",
+	})
+	defer liblog.Sync()
+
+	l := log.Scoped("foo", "an example logger")
+
+	// print diagnostics
+	config := []log.Field{}
+	for _, k := range []string{
+		log.EnvDevelopment,
+		log.EnvLogFormat,
+		log.EnvLogLevel,
+		log.EnvLogScopeLevel,
+		log.EnvLogSamplingInitial,
+		log.EnvLogSamplingThereafter,
+	} {
+		config = append(config, log.String(k, os.Getenv(k)))
+	}
+	l.Info("configuration", config...)
+
+	// sample message
+	l.Warn("hello world!", log.Time("now", time.Now()))
+}
