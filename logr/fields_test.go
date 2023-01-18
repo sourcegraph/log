1f112b1acd49d69d2f6620d6729519574a33e2ab
Robert Lin <robert@bobheadxi.dev>
2023-01-18 10:51:09 -0800

    add non-string test case

--- logr/fields_test.go
+++ logr/fields_test.go
@@ -11,9 +11,11 @@ func TestToLogFields(t *testing.T) {
 	fields := toLogFields([]any{
 		"hello", "world",
 		"goodbye", "bob",
+		"lucky_number", 3,
 	})
 	assert.Equal(t, fields, []log.Field{
 		log.String("hello", "world"),
 		log.String("goodbye", "bob"),
+		log.Int("lucky_number", 3),
 	})
 }
